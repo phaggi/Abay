e7efa9e8eb6f8910a0730e6eed6e44c37270ce78
phaggi <phaggi@gmail.com>
2020-05-11 19:03:58 +0300

    minor changes in listen.py (test=True, checking type audio)

--- Danelia/listen.py
+++ Danelia/listen.py
@@ -1,6 +1,6 @@
 import speech_recognition as sr
 from Danelia.speak import talk
-
+test = False
 
 def commands():
     r = sr.Recognizer()
@@ -12,16 +12,19 @@ def commands():
 
         r.adjust_for_ambient_noise(source, duration=1)
         audio = r.listen(source)
-        print(audio)
-    try:
-        exersize = r.recognize_google(audio, language="ru-Ru").lower()
-        print('Вы сказали: ' + exersize)
-    except sr.UnknownValueError:
-        talk('Я вас не поняла. Повторите пожалуйста.')
-        exersize = commands()
+        if test:
+            assert isinstance(audio, sr.AudioData)
+            print(audio)
+        try:
+            exercise = r.recognize_google(audio, language="ru-Ru").lower()
+            print('Вы сказали: ' + exercise)
+        except sr.UnknownValueError:
+            talk('Я вас не поняла. Повторите пожалуйста.')
+            exercise = commands()
 
-    return exersize
+    return exercise
 
 
 if __name__ == '__main__':
+    test = True
     commands()
