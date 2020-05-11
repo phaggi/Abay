d073056de1dcfa795f9d0918ad15f48f36d9ac54
phaggi <phaggi@gmail.com>
2020-05-11 18:24:04 +0300

    minor changes in listen.py

--- Danelia/listen.py
+++ Danelia/listen.py
@@ -1,17 +1,27 @@
 import speech_recognition as sr
+from Danelia.speak import talk
+
+
 def commands():
     r = sr.Recognizer()
 
     with sr.Microphone() as source:
-        print('Говорите : ')
+        print('Говорите: ')
         r.pause_threshold = 1
+        r.dynamic_energy_threshold = True
+
         r.adjust_for_ambient_noise(source, duration=1)
         audio = r.listen(source)
+        print(audio)
     try:
         exersize = r.recognize_google(audio, language="ru-Ru").lower()
-        print('Вы сказали : ' + exersize)
+        print('Вы сказали: ' + exersize)
     except sr.UnknownValueError:
-        talk('Я вас не поняла , повторите пожалуйста ')
+        talk('Я вас не поняла. Повторите пожалуйста.')
         exersize = commands()
 
     return exersize
+
+
+if __name__ == '__main__':
+    commands()
